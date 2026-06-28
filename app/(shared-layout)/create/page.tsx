e7264aae9d8b64703f79d061423142c7bed0ea56
Amir Azadeh Ranjbar <78546538+amirazadehranjbar@users.noreply.github.com>
2026-06-27 23:16:32 -0700

    add inputs and controllers to create post page

--- app/(shared-layout)/create/page.tsx
+++ app/(shared-layout)/create/page.tsx
@@ -4,20 +4,30 @@ import {Card, CardContent, CardDescription, CardTitle} from "@/components/ui/car
 import {Controller, useForm} from "react-hook-form";
 import {standardSchemaResolver} from "@hookform/resolvers/standard-schema";
 import {PostSchema} from "@/app/schemas/postSchema";
+import {Field, FieldError, FieldGroup, FieldLabel} from "@/components/ui/field";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {Textarea} from "@/components/ui/textarea";
 
 function CreatePage() {
 
     const form = useForm({
-        resolver:standardSchemaResolver(PostSchema),
-        defaultValues :{title: "" , content: ""}
-    })
+        resolver: standardSchemaResolver(PostSchema),
+        defaultValues: {title: "", content: ""}
+    });
+
+
+    const onSubmit = () => {
+        console.log("submmit");
+    }
 
 
     return (
         <div className="h-full bg-chart-5 flex flex-col items-center">
 
             <div className="pt-3">
-                <h1 className="text-2xl md:text-3xl font-extrabold font-mono text-chart-1 tracking-tighter">create post</h1>
+                <h1 className="text-2xl md:text-3xl font-extrabold font-mono text-chart-1 tracking-tighter">create
+                    post</h1>
 
                 <p className="text-chart-1/40">create your own blog and post here</p>
             </div>
@@ -26,7 +36,44 @@ function CreatePage() {
                 <CardTitle className="text-chart-1">new post</CardTitle>
                 <CardDescription>post your blog here</CardDescription>
                 <CardContent>
-                    <form></form>
+                    <form onSubmit={form.handleSubmit(onSubmit)}>
+
+                        <FieldGroup>
+                            <Controller
+                                name="title"
+                                control={form.control}
+                                render={({field, fieldState}) => (
+                                    <Field data-invalid={!!fieldState.error}>
+                                        <FieldLabel htmlFor={field.name}>Title</FieldLabel>
+                                        <Input aria-invalid={fieldState.invalid} placeholder="title"
+                                               id={field.name} {...field}
+                                               className="p-2 border-2 outline-0 border-border focus:border-ring/30 mb-2 shadow-lg shadow-chart-4"/>
+                                        <FieldError errors={fieldState.error ? [fieldState.error] : []}/>
+                                    </Field>
+                                )}
+                            />
+
+
+                            <Controller
+                                name="content"
+                                control={form.control}
+                                render={({field, fieldState}) => (
+                                    <Field data-invalid={!!fieldState.error}>
+                                        <FieldLabel htmlFor={field.name}>Content</FieldLabel>
+                                        <Textarea aria-invalid={fieldState.invalid} placeholder="title"
+                                                  id={field.name} {...field}
+                                                  className="p-2 border-2 outline-0 border-border mb-2 shadow-lg shadow-chart-4"/>
+                                        <FieldError errors={fieldState.error ? [fieldState.error] : []}/>
+                                    </Field>
+                                )}
+                            />
+                        </FieldGroup>
+
+                        <Button className="w-full" type="submit">
+                            post
+                        </Button>
+
+                    </form>
                 </CardContent>
             </Card>
 
