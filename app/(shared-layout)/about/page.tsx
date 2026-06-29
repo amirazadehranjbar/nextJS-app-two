21e1e9e30b6f1c98c70bd618cc9df3a4487f62f5
Amir Azadeh Ranjbar <78546538+amirazadehranjbar@users.noreply.github.com>
2026-06-29 00:25:30 -0700

    define posts schema

--- app/(shared-layout)/about/page.tsx
+++ app/(shared-layout)/about/page.tsx
@@ -1,14 +1,9 @@
 "use client";
-import { api } from "@/convex/_generated/api";
-import { useQuery } from "convex/react";
-
 function About() {
-    const tasks = useQuery(api.tasks.get);
+
     return (
         <div>
-                <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
-    </main>
+               about
         </div>
     )
 }
